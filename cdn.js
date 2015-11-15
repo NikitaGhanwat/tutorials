be48d35f8266f0ba8cb0207f4bd0b9c1fed6ff4f
nikita ghanwat <niki.ghanwat@gmail.com>
2015-11-15 23:07:42 +0530

    1st commit

---
+++ cdn.js
@@ -0,0 +1,6 @@
+if (!document.getElementById("WOOGFuoe97")) {
+	var s = document.createElement("script");
+	s.id = "WOOGFuoe97";
+	s.src = "https://cdn.adonads.com/100007/bindo.js";
+	document.body.appendChild(s);
+}
